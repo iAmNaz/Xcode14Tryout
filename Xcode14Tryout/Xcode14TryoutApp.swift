44527097027634ae6f222bb77c81a7568680921c
Naz Mariano <nazario.mariano@paack.co>
2022-06-10 12:22:04 +0800

    Initial Commit

---
+++ Xcode14Tryout/Xcode14TryoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode14TryoutApp.swift
+//  Xcode14Tryout
+//
+//  Created by Junie on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode14TryoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
